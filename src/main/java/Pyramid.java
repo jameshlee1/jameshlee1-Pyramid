52d9a5f0a4396c768ac0a8330541a5702dbce3d0
James Lee <james.h.lee3@outlook.com>
2025-03-14 18:32:56 +0000

    Coding Challenge Pyramid Complete

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,15 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        StringBuilder pyramid = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                pyramid.append("*");
+            }
+            if (i < n) {
+                pyramid.append("\n");
+            }
+        }
+        return pyramid.toString();
     }
 }
